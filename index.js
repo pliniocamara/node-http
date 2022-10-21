8e443c39d67b594605ac10fba548c5b8a254fde4
pliniocamara <plinio.camara@outlook.com>
2022-10-20 21:15:40 -0300

    aprofundando post e delete

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 //import express from 'express';
+function removeItem(arr, prop, value) {
+    return arr.filter(i => i[prop] !== value);
+}
+
 const express = require('express');
 const app = express();
 
-const books = [
+let books = [
     {_id: 1, title: 'Harry Potter 1', autor: 'J.K. Rowling', favorito: true},
     {_id: 2, title: 'Harry Potter 2', autor: 'J.K. Rowling', favorito: false},
     {_id: 3, title: 'Harry Potter 3', autor: 'J.K. Rowling', favorito: true}
@@ -41,9 +45,21 @@ app.get('/books/:id', (req, res) => {
 });
 
 app.post('/books', (req, res) => {
-    books.push(req.body);
-    res.json(books);
+    const body = (req.body);
+    if (Array.isArray(body)) {
+        body.map(obj => books.push(obj));
+    } else {
+        books.push(req.body);
+    }
+    res.send(books);
 })
+
+app.delete('/books/:id', (req, res) => {
+    const id = +(req.params.id);
+    const arr = removeItem(books, '_id', id);
+    books = arr;
+    res.send(books);
+});
 
 // const hostname = '192.168.2.26';
 // app.listen(3000, hostname);
